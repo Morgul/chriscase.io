2c8cc0e3b7d9b61c91d9dace5066a5ce2989941a
Christopher S. Case <chris.case@g33xnexus.com>
2015-02-26 16:55:19 -0600

    added highlightjs support.

--- client/app.js
+++ client/app.js
@@ -30,6 +30,20 @@ angular.module('chriscaseio', [
             .when('/admin/blog', { templateUrl: '/components/admin/blog/add_edit.html', controller: 'AddEditArticleController' })
             .when('/admin/blog/:slug*', { templateUrl: '/components/admin/blog/add_edit.html', controller: 'AddEditArticleController' })
             .otherwise({redirectTo: '/'});
+
+        // Configure marked parser
+        marked.setOptions({
+            gfm: true,
+            tables: true,
+            breaks: false,
+            pedantic: false,
+            sanitize: false,
+            smartLists: true,
+            smartypants: false,
+            highlight: function (code, lang) {
+                return hljs.highlightAuto(code, [lang]).value;
+            }
+        });
     }]);
 
 // ---------------------------------------------------------------------------------------------------------------------
